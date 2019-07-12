74efbad6d6449d3a4eb021b7ac30bc35934ca048
Ramandeep Malhotra <rmalhotra7@sapient.com>
2019-07-12 11:09:30 +0530

    added collection item to show shop page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import {Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import HatsPage from './pages/hatspage/hatspage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
